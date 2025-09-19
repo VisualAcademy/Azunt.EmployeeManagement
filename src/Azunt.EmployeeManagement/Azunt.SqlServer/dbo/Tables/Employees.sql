81b3d0cf1efe297c44fc7f4fd2b00de23dc33ed5
Park YongJune <redplus@redplus.net>
2025-09-20 02:34:21 +0900

    Add Employees table script to SQL Server project

---
+++ src/Azunt.EmployeeManagement/Azunt.SqlServer/dbo/Tables/Employees.sql
@@ -0,0 +1,12 @@
+﻿--[0][0] 직원: Employees 
+CREATE TABLE [dbo].[Employees]
+(
+    [Id]        BIGINT             IDENTITY (1, 1) NOT NULL PRIMARY KEY,    -- 직원 고유 아이디, 자동 증가
+    [Active]    BIT                DEFAULT ((1)) NULL,                      -- 활성 상태 표시, 기본값 1 (활성)
+    [CreatedAt] DATETIMEOFFSET NULL DEFAULT SYSDATETIMEOFFSET(),            -- 레코드 생성 시간
+    [CreatedBy] NVARCHAR (255)     NULL,                                    -- 레코드 생성자 이름
+    [Name]      NVARCHAR (MAX)     NULL,                                    -- 이름
+    ---
+    [FirstName] NVARCHAR (255)     NULL,                                    -- 이름 (First Name)  
+    [LastName]  NVARCHAR (255)     NULL,                                    -- 성 (Last Name
+);
