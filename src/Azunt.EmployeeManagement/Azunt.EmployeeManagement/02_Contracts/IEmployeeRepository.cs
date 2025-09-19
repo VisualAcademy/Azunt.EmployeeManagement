f4e0b1affa8536e3e2242a05c376128f21c1c055
Park YongJune <redplus@redplus.net>
2025-09-20 05:26:09 +0900

    Add IEmployeeRepository interface with async CRUD and paging methods

---
+++ src/Azunt.EmployeeManagement/Azunt.EmployeeManagement/02_Contracts/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using Azunt.Models.Common;
+
+namespace Azunt.EmployeeManagement;
+
+public interface IEmployeeRepository
+{
+    Task<Employee> AddAsync(Employee model, string? connectionString = null);
+    Task<List<Employee>> GetAllAsync(string? connectionString = null);
+    Task<Employee> GetByIdAsync(long id, string? connectionString = null);
+    Task<bool> UpdateAsync(Employee model, string? connectionString = null);
+    Task<bool> DeleteAsync(long id, string? connectionString = null);
+    Task<ArticleSet<Employee, int>> GetAllAsync<TParentIdentifier>(int pageIndex, int pageSize, string searchField, string searchQuery, string sortOrder, TParentIdentifier parentIdentifier, string? connectionString = null);
+}
